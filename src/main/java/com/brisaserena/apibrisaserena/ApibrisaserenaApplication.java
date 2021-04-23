d0faac886b143022e734f3fa09c7d10c0524df36
henriquesneiva <henriqueseniva@gmail.com>
2021-04-23 16:50:59 -0300

    criando o projeto

---
+++ src/main/java/com/brisaserena/apibrisaserena/ApibrisaserenaApplication.java
@@ -0,0 +1,13 @@
+package com.brisaserena.apibrisaserena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibrisaserenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibrisaserenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brisaserena/apibrisaserena/ApibrisaserenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brisaserena.apibrisaserena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibrisaserenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
